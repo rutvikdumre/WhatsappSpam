c0d2fc67c8bfac4e81a9d6ba6e486444a38054a4
Rutvik Dumre <rutvikdumre@gmail.com>
2020-04-20 01:06:45 +0530

    final

--- spam.py
+++ spam.py
@@ -23,21 +23,26 @@ t=int(input('Enter no of times you want to spam this message:'))
 driver = webdriver.Chrome('chromedriver') 
   
 driver.get("https://web.whatsapp.com/") 
-wait = WebDriverWait(driver, 600) 
+wait = WebDriverWait(driver, 10) 
   
 
 print(target)
     
 
 
-for j in target:  
-    x_arg = '//span[contains(@title,'+ j +')]'
-    group_title = wait.until(EC.presence_of_element_located(( 
-        By.XPATH, x_arg))) 
-    group_title.click() 
-    inp_xpath = '//div[@dir="ltr"][@data-tab="1"][@spellcheck="true"]'
-    input_box = wait.until(EC.presence_of_element_located(( 
-        By.XPATH, inp_xpath))) 
-    for i in range(t): 
-        input_box.send_keys(string + Keys.ENTER) 
-        time.sleep(1) 
+for j in target:
+    #time.sleep(2)
+    try:
+        x_arg = '//span[contains(@title,'+ j +')]'
+        print(x_arg)
+        group_title = wait.until(EC.presence_of_element_located(( 
+            By.XPATH, x_arg))) 
+        group_title.click() 
+        inp_xpath = '//div[@dir="ltr"][@data-tab="1"][@spellcheck="true"]'
+        input_box = wait.until(EC.presence_of_element_located(( 
+            By.XPATH, inp_xpath))) 
+        for i in range(t): 
+            input_box.send_keys(string + Keys.ENTER) 
+            time.sleep(1)
+    except:
+        print(j + ' Not found')
